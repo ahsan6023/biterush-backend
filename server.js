e758da649e262c61c76a65dcfa9448f955c4853d
ahsan6023 <itxahsan845@gmail.com>
2026-05-19 08:03:49 -0700

    Update CORS origin to production URL

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'https://ahsanfastfood.netlify.app',
   credentials: true
 }));
 app.use(express.json());
